1659158ad5c27ae55342e44a4a8e270d5a72716f
Yeung <498832514@qq.com>
2025-11-20 17:39:23 +0800

    chore: config文件修改

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 关键配置
+  images: { unoptimized: true }, // 可选：禁用图片优化
 };
 
 export default nextConfig;
